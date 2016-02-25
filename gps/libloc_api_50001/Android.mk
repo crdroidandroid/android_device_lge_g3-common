8f2e4d6570ffc40e82cc460895858a8aa2fde87d
Aurimas Liutikas <aurimas@google.com>
2016-02-24 17:40:49 -0800

    g3-common: gps: Suppress unused-parameter warnings
    Bug: 26936282
    Change-Id: I7fdf58d232a59c0ba0d5f52c0f958f6eab0c4514

--- gps/libloc_api_50001/Android.mk
+++ gps/libloc_api_50001/Android.mk
@@ -36,7 +36,8 @@ LOCAL_SRC_FILES += \
 
 LOCAL_CFLAGS += \
      -fno-short-enums \
-     -D_ANDROID_
+     -D_ANDROID_ \
+     -Wno-unused-parameter
 
 LOCAL_C_INCLUDES:= \
     $(TARGET_OUT_HEADERS)/gps.utils \
@@ -81,7 +82,8 @@ LOCAL_SRC_FILES += \
 
 LOCAL_CFLAGS += \
     -fno-short-enums \
-    -D_ANDROID_ \
+     -D_ANDROID_ \
+     -Wno-unused-parameter
 
 ifeq ($(TARGET_USES_QCOM_BSP), true)
 LOCAL_CFLAGS += -DTARGET_USES_QCOM_BSP
