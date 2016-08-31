ea4a42c2c5b93630f5c10d1ee3edae40758f694d
LuK1337 <priv.luk@gmail.com>
2016-08-31 11:42:24 +0200

    g3: Build telephony-ext
    Change-Id: Ic5c0404057fec4f68f6a6d52fca3318e94245597

--- g3.mk
+++ g3.mk
@@ -188,6 +188,10 @@ PRODUCT_PACKAGES += \
     init.sensors.sh \
     ueventd.g3.rc
 
+# Telephony-ext
+PRODUCT_PACKAGES += telephony-ext
+PRODUCT_BOOT_JARS += telephony-ext
+
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-8974.conf:system/etc/thermal-engine-8974.conf
