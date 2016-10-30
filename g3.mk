6dea04cc239912f7efa84fa7fc84736614ed1247
Ethan Chen <intervigil@gmail.com>
2016-10-30 12:44:42 +0200

    g3-common: Add GraphicBuffer shim for camera.vendor.msm8974.so
    Change-Id: Idb1860a05b3669581951ac303bdefbb8ff0c8a99

--- g3.mk
+++ g3.mk
@@ -99,6 +99,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl-legacy \
     camera.device@1.0-impl-legacy \
+    libshim_camera \
     Snap \
     camera.msm8974
 
