0203de9bc107fbf5a4f209f8a1a5b75fca395f79
jrior001 <jriordan001@gmail.com>
2016-12-29 12:18:09 -0500

    g3: utilize common Lid App
    * remove quickcircle app
    * build common FlipFlap app
    * add overlays for FlipFlap
    
    Change-Id: I734d5d3e12408eb491d32838de5b57e51ad4d2d8

--- g3.mk
+++ g3.mk
@@ -139,6 +139,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
+# QuickCircle Case App
+PRODUCT_PACKAGES += \
+    FlipFlap
+
 # NFC
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
