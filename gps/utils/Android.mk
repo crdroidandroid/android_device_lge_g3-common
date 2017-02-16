048e9fed1d9484615637005d4ae5d87756b81a86
Vijay Venkatraman <vijaykv@google.com>
2017-02-16 15:35:58 -0800

    g3-common: gps: Replacing copy headers with header libraries
    Bug: 33241851
    Test: Build target and pass CTS location tests
    Change-Id: I75db5e6de3c4194a6a46e0d37fcd899ea1d1db04

--- gps/utils/Android.mk
+++ gps/utils/Android.mk
@@ -34,20 +34,6 @@ LOCAL_LDFLAGS += -Wl,--export-dynamic
 LOCAL_C_INCLUDES:= \
     $(LOCAL_PATH)/../platform_lib_abstractions
 
-LOCAL_COPY_HEADERS_TO:= gps.utils/
-LOCAL_COPY_HEADERS:= \
-   loc_log.h \
-   loc_cfg.h \
-   log_util.h \
-   linked_list.h \
-   msg_q.h \
-   loc_target.h \
-   loc_timer.h \
-   ../platform_lib_abstractions/platform_lib_includes.h \
-   ../platform_lib_abstractions/platform_lib_time.h \
-   ../platform_lib_abstractions/platform_lib_macros.h \
-   loc_misc_utils.h
-
 LOCAL_MODULE := libgps.utils
 LOCAL_MODULE_OWNER := qcom
 LOCAL_PROPRIETARY_MODULE := true
@@ -57,3 +43,8 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_PRELINK_MODULE := false
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libgps.utils_headers
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH) $(LOCAL_PATH)/../platform_lib_abstractions
+include $(BUILD_HEADER_LIBRARY)
