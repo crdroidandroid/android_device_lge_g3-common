e4ab9bb614d25651ba58040109db584bc7242ff0
Simon Shields <simon@lineageos.org>
2017-08-24 00:23:09 +1000

    g3-common: CameraWrapper: Add missing libs for headers
    Change-Id: Ifdaf82855e9ccc3b9d8e13f4124e3670b803109d

--- camera/Android.mk
+++ camera/Android.mk
@@ -5,7 +5,9 @@ LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libgui libutils libhidltransport android.hidl.token@1.0-utils
+    libhardware liblog libcamera_client libgui libutils libhidltransport \
+    android.hidl.token@1.0-utils \
+    android.hardware.graphics.bufferqueue@1.0
 
 LOCAL_STATIC_LIBRARIES := \
     libarect libbase
