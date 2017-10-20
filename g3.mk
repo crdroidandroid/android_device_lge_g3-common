5e5e75ddcabd510746cf862731ba209b57cbb447
jrior001 <jriordan001@gmail.com>
2017-10-19 23:00:45 -0400

    g3-common: convert CameraParameters header to shim lib
    Change-Id: I31c7c3346c8fac4020cd86822c9cfb1a6db26b77

--- g3.mk
+++ g3.mk
@@ -100,6 +100,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl-legacy \
     camera.device@1.0-impl-legacy \
     libshim_camera \
+    libshim_camera_parameters \
     Snap \
     camera.msm8974
 
