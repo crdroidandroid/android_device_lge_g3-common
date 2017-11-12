925f7e860595969cdcb6371ef9c41026b0b3f409
firebird11 <hbgassel@gmail.com>
2017-11-12 12:10:44 +0100

    Revert "g3: Remove FlipFlap dependency until it is updated for oreo"
    This reverts commit 4bc563e5acc2d4bfb1eaa647cc76af8a48add20e.
    
    Change-Id: I5eb266f7812c259f2e86c5e60e7c5e830d145271

--- g3.mk
+++ g3.mk
@@ -116,6 +116,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl
 
+# FlipFlap
+PRODUCT_PACKAGES += \
+    FlipFlap
+
 # IPv6 tethering
 PRODUCT_PACKAGES += \
     ebtables \
