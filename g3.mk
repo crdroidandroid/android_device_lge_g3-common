c455c1ce9452286119f426d08b929c217aec8dfe
Bruno Martins <bgcngm@gmail.com>
2017-12-13 10:32:07 +0100

    g3-common: Include DRM service
    Change-Id: I4999809c288c84d6e9c39383ff76f9ee85a97cae

--- g3.mk
+++ g3.mk
@@ -115,7 +115,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
 
 # FlipFlap
 PRODUCT_PACKAGES += \
