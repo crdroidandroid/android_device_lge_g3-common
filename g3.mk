ea9bb65d5b89cfa85b7edb3354a2e5e579d08b64
ZjemCiKolege <frager1911@gmail.com>
2018-03-06 07:19:46 +0000

    g3-common: Add back hostapd
    cause problem with wifi tethering
    
    Change-Id: Id97adf5942a5696488bd8fe334b90ef70f56807e

--- g3.mk
+++ g3.mk
@@ -264,7 +264,7 @@ PRODUCT_PACKAGES += \
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
-    wificond \
+    hostapd \
     libwpa_client \
     wpa_supplicant \
     wpa_supplicant.conf
