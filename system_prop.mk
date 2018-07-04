a9bf0d0fde3cf43421c611a62d3b66a4c2b5c634
firebird11 <hbgassel@gmail.com>
2018-07-04 09:40:40 +0200

    g3-common: Set vendor security patch level
    matching 30o
    
    Change-Id: Ic7913cd741a8246a5edaef21bcbb114915e2acae

--- system_prop.mk
+++ system_prop.mk
@@ -51,3 +51,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.qualcomm.sns.daemon=e \
     debug.qualcomm.sns.hal=e \
     debug.qualcomm.sns.libsensor1=e
+
+# Vendor security patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lineage.build.vendor_security_patch=2016-08-01
