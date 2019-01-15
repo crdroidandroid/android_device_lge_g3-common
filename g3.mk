4299399592667f3eabb6e73c6d719f8647adcd92
Daniel Jarai <jaraidaniel@gmail.com>
2019-01-15 16:09:37 +0100

    g3-common: Make the external camera provider ignore internal cameras
     * The external camera provider occupies our camera v4l2 nodes,
       potentially crashing the kernel driver and blocking the camera HAL.
       Unfortunately, there's no easy way to just disable it, so let's just
       tell it to ignore the internal video devices.
    
    Change-Id: I00c67de1b94a1f7eca1bf458d1d0e4de3507aa81
    Signed-off-by: Daniel Jarai <jaraidaniel@gmail.com>

--- g3.mk
+++ g3.mk
@@ -105,6 +105,9 @@ PRODUCT_PACKAGES += \
     Snap \
     camera.msm8974
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
