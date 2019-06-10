0f64293a6a9e0320a7656f7801395aac08e35652
Kevin Rocard <krocard@google.com>
2019-06-10 08:46:29 +0200

    g3-common: Uprev audio to 4.0
     * Also, add audio@2.0 service
    
    Test: regression test on the board
    Bug: 38184704
    Change-Id: Ife559125850277865451f92ea862e7ec47fc9da7
    Merged-In: Ife559125850277865451f92ea862e7ec47fc9da7
    Cherry-picked from master
    Signed-off-by: Kevin Rocard <krocard@google.com>

--- g3.mk
+++ g3.mk
@@ -65,8 +65,9 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio@4.0-impl \
+    android.hardware.audio@2.0-service \
+    android.hardware.audio.effect@4.0-impl \
     audio.primary.msm8974 \
     audio_policy.msm8974 \
     audio.a2dp.default \
